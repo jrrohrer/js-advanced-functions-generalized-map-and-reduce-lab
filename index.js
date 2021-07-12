aa5ec5a6ac63c0ecacccf401651c27f21fc6e914
jrrohrer <jessie.rohrer@gmail.com>
2021-07-12 14:57:01 -0400

    commit tree 614c60e8d9ca6ed23b60609cefab9429a193897e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(arr, callback) {
+  const a = [];
+  for (let i = 0; i < arr.length; i++) {
+    const arrElement = arr[i];
+    a.push(callback(arrElement));
+  }
+  return a;
+}
+
+function reduce(arr, callback, start) {
+  let total;
+  if (start) {
+    total = start;
+    for (let i = 0; i < arr.length; i++) {
+      total = callback(arr[i], total);
+    }
+    return total;
+  } else {
+    total = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      total = callback(arr[i], total);
+    }
+    return total;
+  }
+}
